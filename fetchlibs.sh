4e2fa20e8706f6f2531cadfb09cc9a8af927bf54
lalkh <547079826@qq.com>
2019-05-13 03:39:29 +0800

    Update fetchlibs.sh

--- fetchlibs.sh
+++ fetchlibs.sh
@@ -64,6 +64,6 @@ fetcharch powerpc ubuntu trusty
 fetcharch arm64 ubuntu trusty
 fetcharch i386 ubuntu trusty
 #fetcharch mips debian jessie
-fetcharch mipsel debian jessie
+#fetcharch mipsel debian jessie
 
 # mini debootstrap 
